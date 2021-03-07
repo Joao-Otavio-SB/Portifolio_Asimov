6a25882b6e1edfa19620389ffb8f6ee3baf99eba
Joao-Otavio-SB <joogui2010@hotmail.com>
2021-03-07 19:46:45 -0300

    add

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Portifolio-asimov
+
+Comecei a fazer algo sério mas no meio do caminho decidi que não, então é um portfólio sem a parte do potfólio.
---
+++ js/script.js
@@ -0,0 +1,13 @@
+function openNav() {
+
+	var x =  document.getElementById("navigation");
+
+	if (x.className === "navigation") {
+		x.className += " menujs";
+
+	} else {
+		x.className = "navigation";
+
+	}
+
+}
