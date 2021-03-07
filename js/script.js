399e413735c23905f8d69bb83db03643734f8581
Joao-Otavio-SB <joogui2010@hotmail.com>
2021-03-07 20:00:17 -0300

    add

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
-function openNav() {
+function openN() {
 
-	var x =  document.getElementById("navigation");
+	var x = document.getElementById("navigation");
 
 	if (x.className === "navigation") {
-		x.className += " menujs";
-
+		x.className += " menuJS";
+        document.getElementById("threelineIcon").innerHTML = "&Cross;";
 	} else {
 		x.className = "navigation";
-
+        document.getElementById("threelineIcon").innerHTML = "&#x2630;";
 	}
 
 }
